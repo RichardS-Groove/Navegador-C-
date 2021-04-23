7ec60fba324b97d166a51033fbf0216b5586539d
RichardS <richardsjcb@gmail.com>
2021-04-23 13:42:38 -0300

    Termino de la segunda clase

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,7 +29,40 @@ namespace Navegador
         /// </summary>
         private void InitializeComponent()
         {
+            this.bBuscar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tBuscar = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // bBuscar
+            // 
+            this.bBuscar.BackColor = System.Drawing.Color.Blue;
+            this.bBuscar.ForeColor = System.Drawing.Color.White;
+            this.bBuscar.Location = new System.Drawing.Point(630, 21);
+            this.bBuscar.Name = "bBuscar";
+            this.bBuscar.Size = new System.Drawing.Size(53, 20);
+            this.bBuscar.TabIndex = 1;
+            this.bBuscar.Text = "Buscar";
+            this.bBuscar.UseVisualStyleBackColor = false;
+            this.bBuscar.Click += new System.EventHandler(this.bBuscar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(34, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Escriba";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // tBuscar
+            // 
+            this.tBuscar.Location = new System.Drawing.Point(110, 22);
+            this.tBuscar.Name = "tBuscar";
+            this.tBuscar.Size = new System.Drawing.Size(495, 20);
+            this.tBuscar.TabIndex = 3;
             // 
             // Form1
             // 
@@ -37,13 +70,21 @@ namespace Navegador
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tBuscar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bBuscar);
             this.Name = "Form1";
             this.Text = "Buscador Google";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.Button bBuscar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tBuscar;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,21 @@ namespace Navegador
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bBuscar_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.google.com/search?q=" + tBuscar.Text + "&sxsrf=ALeKk01gI0283zoM3QOUq7-XI97jtHdyig%3A1619195400465&source=hp&ei=CPaCYIuVGorR1sQPo72QwAw&iflsig=AINFCbYAAAAAYIMEGMtNc53Y-v9yo6qA60RoEbBFWt_Q&oq=" + tBuscar.Text + "&gs_lcp=Cgdnd3Mtd2l6EAMyBAgjECcyBQguELEDMgUILhCxAzIICAAQsQMQgwEyBQgAELEDMggIABCxAxCDATIFCC4QsQMyAggAMgUILhCxAzIICAAQsQMQgwE6CwgAELEDEMcBEKMCOggILhCxAxCDAVC4qAJY26wCYLGuAmgAcAB4AIABkgGIAbUEkgEDMS40mAEAoAEBqgEHZ3dzLXdpeg&sclient=gws-wiz&ved=0ahUKEwiLuaj15JTwAhWKqJUCHaMeBMgQ4dUDCAk&uact=5");
+        }
+
     }
 }
